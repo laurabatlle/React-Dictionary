fd0402145225eedea976c6735e80e853b269a749
laurabatlle <laura.batlle007@gmail.com>
2023-05-20 10:30:48 +0200

    tried if bootstrap is working

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>React dictionary</p>
-        <a className="btn btn-primary">hello</a>
+        <p className="btn btn-primary">hello</p>
       </header>
     </div>
   );
