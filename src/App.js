57b6dbec8552ce037e63afd7611b5680448837f8
laurabatlle <laura.batlle007@gmail.com>
2023-05-30 19:52:18 +0200

    created search engine and Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>React dictionary</p>
-        <p className="btn btn-primary">hello</p>
-      </header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <p>React dictionary</p>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer> Coded by Laura Batlle </footer>
     </div>
   );
 }
